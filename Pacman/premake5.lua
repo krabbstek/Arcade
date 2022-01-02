2a7380a2c08c59125462fba96f944e19482deab7
krabbstek <aandersson.axel@gmail.com>
2022-01-02 16:00:25 +0100

    Added SDL basic window

--- Pacman/premake5.lua
+++ Pacman/premake5.lua
@@ -1,4 +1,5 @@
 project "Pacman"
+    location "Pacman"
     kind "ConsoleApp"
     language "C++"
     staticruntime "Off"
@@ -10,13 +11,42 @@ project "Pacman"
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
     files {
-        "*.h",
-        "*.cpp",
+        "**.h",
+        "**.cpp",
     }
 
     includedirs {
         "%{prj.location}",
+        "%{wks.location}/SDL2/include",
     }
+
+    links {
+        "SDL2",
+    }
+
+    defines {
+        "SDL_MAIN_HANDLED",
+    }
+
+    filter "architecture:x86"
+        libdirs {
+            "%{wks.location}/SDL2/lib/x86",
+        }
+
+    filter "architecture:x64"
+        libdirs {
+            "%{wks.location}/SDL2/lib/x64",
+        }
+
+    filter { "system:windows", "architecture:x86" }
+        postbuildcommands {
+            ("{COPY} %{wks.location}/SDL2/lib/x86/SDL2.dll %{cfg.targetdir}"),
+        }
+
+    filter { "system:windows", "architecture:x64" }
+        postbuildcommands {
+            ("{COPY} %{wks.location}/SDL2/lib/x64/SDL2.dll %{cfg.targetdir}"),
+        }
 
     filter "configurations:Debug"
         buildoptions "/MDd"
--- Pacman/src/Pacman.cpp
+++ Pacman/src/Pacman.cpp
@@ -1,8 +1,56 @@
 #include <iostream>
+#include <SDL.h>
 
 int main(int argc, const char* argv[])
 {
-    std::printf("Pacman\n");
-    std::cin.get();
-    return 0;
+    int initResult;
+
+    initResult = SDL_Init(SDL_INIT_EVERYTHING);
+    if (initResult)
+    {
+        std::cerr << "Failed to initialize SDL\n";
+        goto Exit;
+    }
+
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    constexpr int width = 480;
+    constexpr int height = 480;
+    uint32_t flags = 0;
+    initResult = SDL_CreateWindowAndRenderer(width, height, flags, &window, &renderer);
+    if (initResult)
+    {
+        std::cerr << "Failed to create SDL window and renderer\n";
+        goto SDL_Quit;
+    }
+    SDL_SetWindowTitle(window, "Pacman");
+
+    bool running = true;
+    SDL_Event sdlEvent;
+    while (running)
+    {
+        while (SDL_PollEvent(&sdlEvent))
+        {
+            switch (sdlEvent.type)
+            {
+            case SDL_QUIT:
+                running = false;
+                break;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
+
+SDL_DestroyWindow:
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+
+SDL_Quit:
+    SDL_Quit();
+    
+Exit:
+    return initResult;
 }
